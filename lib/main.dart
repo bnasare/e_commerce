6fb31348059b5df704c4ec5451f5936813448513
bnasare <124492150+bnasare@users.noreply.github.com>
2023-09-14 22:41:23 +0000

    firebase set up

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ import 'package:e_commerce/screens/bottom_bar_screen.dart';
 import 'package:e_commerce/screens/orders/orders_screen.dart';
 import 'package:e_commerce/screens/viewed_recently/viewed_recently_screen.dart';
 import 'package:e_commerce/screens/wishlist/wishlist_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
@@ -49,50 +50,74 @@ class _MyAppState extends State<MyApp> {
     super.initState();
   }
 
+  final firebaseInitialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) {
-          return themeChangeProvider;
-        }),
-        ChangeNotifierProvider(create: (_) {
-          return ProductProvider();
-        }),
-        ChangeNotifierProvider(create: (_) {
-          return CartProvider();
-        }),
-        ChangeNotifierProvider(create: (_) {
-          return WishListProvider();
-        }),
-        ChangeNotifierProvider(create: (_) {
-          return ViewedProductsProvider();
-        })
-      ],
-      child: Consumer<DarkThemeProvider>(
-        builder: (context, themeProvider, child) {
-          return MaterialApp(
-            home: const BottomBarScreen(),
-            debugShowCheckedModeBanner: false,
-            theme: Styles.themeData(themeProvider.getDarkTheme, context),
-            routes: {
-              OnSaleScreen.routeName: (context) => const OnSaleScreen(),
-              FeedsScreen.routeName: (context) => const FeedsScreen(),
-              ProductDetailsScreen.routeName: (context) =>
-                  const ProductDetailsScreen(),
-              WishListScreen.routeName: (context) => const WishListScreen(),
-              OrdersScreen.routeName: (context) => const OrdersScreen(),
-              ViewedRecentlyScreen.routeName: (context) =>
-                  const ViewedRecentlyScreen(),
-              SignUpScreen.routeName: (context) => const SignUpScreen(),
-              LoginScreen.routeName: (context) => const LoginScreen(),
-              ForgetPasswordScreen.routeName: (context) =>
-                  const ForgetPasswordScreen(),
-              CategoryScreen.routeName: (context) => const CategoryScreen()
-            },
+    return FutureBuilder(
+      future: firebaseInitialization,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const MaterialApp(
+            home: Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
           );
-        },
-      ),
+        } else {
+          const MaterialApp(
+            home: Scaffold(
+              body: Center(
+                child: Text('An error occured'),
+              ),
+            ),
+          );
+        }
+        return MultiProvider(
+          providers: [
+            ChangeNotifierProvider(create: (_) {
+              return themeChangeProvider;
+            }),
+            ChangeNotifierProvider(create: (_) {
+              return ProductProvider();
+            }),
+            ChangeNotifierProvider(create: (_) {
+              return CartProvider();
+            }),
+            ChangeNotifierProvider(create: (_) {
+              return WishListProvider();
+            }),
+            ChangeNotifierProvider(create: (_) {
+              return ViewedProductsProvider();
+            })
+          ],
+          child: Consumer<DarkThemeProvider>(
+            builder: (context, themeProvider, child) {
+              return MaterialApp(
+                home: const BottomBarScreen(),
+                debugShowCheckedModeBanner: false,
+                theme: Styles.themeData(themeProvider.getDarkTheme, context),
+                routes: {
+                  OnSaleScreen.routeName: (context) => const OnSaleScreen(),
+                  FeedsScreen.routeName: (context) => const FeedsScreen(),
+                  ProductDetailsScreen.routeName: (context) =>
+                      const ProductDetailsScreen(),
+                  WishListScreen.routeName: (context) => const WishListScreen(),
+                  OrdersScreen.routeName: (context) => const OrdersScreen(),
+                  ViewedRecentlyScreen.routeName: (context) =>
+                      const ViewedRecentlyScreen(),
+                  SignUpScreen.routeName: (context) => const SignUpScreen(),
+                  LoginScreen.routeName: (context) => const LoginScreen(),
+                  ForgetPasswordScreen.routeName: (context) =>
+                      const ForgetPasswordScreen(),
+                  CategoryScreen.routeName: (context) => const CategoryScreen()
+                },
+              );
+            },
+          ),
+        );
+      },
     );
   }
 }
